1b76e1a0a4f3018b07386c1b37ea4f9f3cd630f6
Aish Waheed <64914670+aish8la@users.noreply.github.com>
2025-09-02 22:50:00 +0500

    feat: move fetching logic and loading state to parent component layout

--- src/components/layout/Layout.jsx
+++ src/components/layout/Layout.jsx
@@ -1,12 +1,44 @@
+import { useState, useEffect } from "react";
 import { Outlet } from "react-router-dom";
 import Header from "./Header";
 
 function Layout() {
+    const [loading, setLoading] = useState(true);
+    const [products, setProducts] = useState([]);
+    
+    useEffect(() => {
+        const controller = new AbortController();
+        const signal = controller.signal;
+        fetch("https://fakestoreapi.com/products", {signal: signal})
+        .then(response => {
+            if(!response.ok) {
+                throw new Error(`Server Error ${response.status}`);
+            }
+            return response.json();
+        })
+        .then( data => { 
+            setProducts(data);
+        })
+        .catch(error => {
+            if(error.name === "AbortError") return
+            console.error("Fetch Failed:", error);
+        })
+        .finally(() => {
+            setLoading(false);
+        });
+
+        return () => {
+            controller.abort();
+        }
+    }, []);
+
     return (
         <>
             <Header />
             <main>
-                <Outlet />
+                {loading ? "Now Loading" : //TODO: add better handling load
+                    <Outlet context={[products]}/>
+                }
             </main>
         </>
     );
--- src/components/pages/ProductListing.jsx
+++ src/components/pages/ProductListing.jsx
@@ -1,42 +1,12 @@
 import Product from "./Product";
-import { useEffect, useState } from "react";
+import { useOutletContext } from "react-router-dom";
 
 function ProductListing() {
-    const [loading, setLoading] = useState(true);
-    const [products, setProducts] = useState([]);
-    
-    useEffect(() => {
-        const controller = new AbortController();
-        const signal = controller.signal;
-        fetch("https://fakestoreapi.com/products", {signal: signal})
-        .then(response => {
-            if(!response.ok) {
-                throw new Error(`Server Error ${response.status}`);
-            }
-            return response.json();
-        })
-        .then( data => { 
-            setProducts(data);
-        })
-        .catch(error => {
-            if(error.name === "AbortError") return
-            console.error("Fetch Failed:", error);
-        })
-        .finally(() => {
-            setLoading(false);
-        });
-
-        return () => {
-            controller.abort();
-        }
-    }, []);
+    const [products] = useOutletContext();
 
     return (
         <div>
-            {!loading ? 
-                products.map(product => <Product key={product.id} product={product}></Product>)
-                : "Loading Page" //TODO: add better handling load
-            }
+            {products.map(product => <Product key={product.id} product={product}></Product>)}
         </div>
     )
 }
