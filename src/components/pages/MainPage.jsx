9000a277a762a9558219ca7174a678e0abdafad9
Aish Waheed <64914670+aish8la@users.noreply.github.com>
2025-09-06 21:33:41 +0500

    refactor: import hero image instead of linking to path

--- src/components/pages/MainPage.jsx
+++ src/components/pages/MainPage.jsx
@@ -1,9 +1,11 @@
-import styles from "./MainPage.module.css"
+import styles from "./MainPage.module.css";
+import hero from "../../assets/hero-img.jpg";
 
 function MainPage() {
+
     return <div>
         <section>
-            <img className={styles.heroImage} src="./src/assets/hero-img.jpg" alt="" />
+            <img className={styles.heroImage} src={hero} alt="" />
         </section>
     </div>
 }
