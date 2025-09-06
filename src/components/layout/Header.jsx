d35c7ea96f3fb3870d1fabe06715e90450eff222
Aish Waheed <64914670+aish8la@users.noreply.github.com>
2025-09-07 02:38:20 +0500

    feat: add styling to header component

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -1,15 +1,19 @@
 import { NavLink } from "react-router-dom";
+import styles from "./Header.module.css";
 
 function Header({ cartCount }) {
 
     return (
-        <header>
-            <span>
-                <NavLink to={"/"}>Home</NavLink>
-                <NavLink to={"about"}>About</NavLink>
-                <NavLink to={"shop"}>Shop</NavLink>
-            </span>
-            <NavLink to={"cart"}>Cart<span>{cartCount}</span></NavLink>
+        <header className={styles.mainHeader}>
+            <h1>Fake Shop</h1>
+            <nav className={styles.navigation}>
+                <span className={styles.navItems}>
+                    <NavLink to={"/"}>Home</NavLink>
+                    <NavLink to={"about"}>About</NavLink>
+                    <NavLink to={"shop"}>Shop</NavLink>
+                </span>
+                <NavLink to={"cart"}>Cart<span>{cartCount}</span></NavLink>
+            </nav>
         </header>
     )
 }
