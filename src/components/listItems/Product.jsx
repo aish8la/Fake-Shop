f02aee8efa1ab9acf7e99770b1464c34c3607bc0
Aish Waheed <64914670+aish8la@users.noreply.github.com>
2025-09-06 21:29:21 +0500

    refactor: change NavLink to Link component for products

--- src/components/listItems/Product.jsx
+++ src/components/listItems/Product.jsx
@@ -1,14 +1,14 @@
-import { NavLink, useOutletContext } from "react-router-dom";
+import { Link, NavLink, useOutletContext } from "react-router-dom";
 
 function Product({ product }) {
     const { addToCart } = useOutletContext();
 
     return (
         <div>
-            <NavLink to={`/product/${product.id}`}>
+            <Link to={`/product/${product.id}`}>
                 <img src={product.image}></img>
                 <p>{product.title}</p>
-            </NavLink>
+            </Link>
             <p>{product.price}</p>
             <button onClick={() => addToCart(product.id)}>Add to Cart</button>
         </div>
