6ada0aba27a3dab8ef8bca624b34a1d238609d50
Aish Waheed <64914670+aish8la@users.noreply.github.com>
2025-09-11 02:27:13 +0500

    feat: add styling to shop page and components

--- src/components/listItems/Product.jsx
+++ src/components/listItems/Product.jsx
@@ -1,16 +1,20 @@
-import { Link, NavLink, useOutletContext } from "react-router-dom";
+import { Link, useOutletContext } from "react-router-dom";
+import styles from "./Product.module.css";
 
 function Product({ product }) {
     const { addToCart } = useOutletContext();
 
     return (
-        <div>
+        <div className={styles.productCard}>
             <Link to={`/product/${product.id}`}>
-                <img src={product.image}></img>
+                <div className={styles.imageContainer}>
+                    <img src={product.image}></img>
+                </div>
+
                 <p>{product.title}</p>
             </Link>
             <p>{product.price}</p>
-            <button onClick={() => addToCart(product.id)}>Add to Cart</button>
+            <button className={styles.addButton} onClick={() => addToCart(product.id)}>Add to Cart</button>
         </div>
     )
 }
--- src/components/pages/ProductListing.jsx
+++ src/components/pages/ProductListing.jsx
@@ -1,11 +1,12 @@
 import Product from "../listItems/Product";
 import { useOutletContext } from "react-router-dom";
+import styles from "./ProductListing.module.css";
 
 function ProductListing() {
     const {products} = useOutletContext();
 
     return (
-        <div>
+        <div className={styles.listingContainer}>
             {products.map(product => <Product key={product.id} product={product}></Product>)}
         </div>
     )
