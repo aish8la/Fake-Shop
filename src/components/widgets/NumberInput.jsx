65b2b02530a4a331e5092ad4e24ba04a3e7ec257
Aish Waheed <64914670+aish8la@users.noreply.github.com>
2025-09-11 20:55:35 +0500

    feat: add minimum and maximum value constraint to number input component

--- src/components/widgets/NumberInput.jsx
+++ src/components/widgets/NumberInput.jsx
@@ -2,11 +2,22 @@ import styles from "./NumberInput.module.css";
 
 function NumberInput({number, setNumber}) {
 
+    function changeNumber(modifier = 1, newNumber = null) {
+        setNumber(prev => {
+            if(newNumber) {
+                newNumber = parseInt(newNumber);
+            } else {
+                newNumber = parseInt(prev) + modifier;
+            }
+            return parseInt(newNumber > 99 || newNumber < 1 ? 1 : newNumber);
+        });
+    }
+
     return (
         <div className={styles.numberInput}>
-            <button onClick={() => setNumber(prev => prev - 1)}>-</button>
-            <input type="number" value={number} onChange={(e) => setNumber(e.target.value)}/>
-            <button onClick={() => setNumber(prev => prev + 1)}>+</button>
+            <button onClick={() => changeNumber(-1)}>-</button>
+            <input type="number" value={number} onChange={(e) => changeNumber(0, e.target.value)} max="1" min="99"/>
+            <button onClick={() => changeNumber(1)}>+</button>
         </div>
     )
 }
