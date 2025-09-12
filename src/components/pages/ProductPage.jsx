cb1c9a796e237fdafb066a7711a25df587182cc9
Aish Waheed <64914670+aish8la@users.noreply.github.com>
2025-09-13 04:11:09 +0500

    feat: add function to add to cart from the product page

--- src/components/pages/ProductPage.jsx
+++ src/components/pages/ProductPage.jsx
@@ -1,17 +1,26 @@
 import { useOutletContext, useParams } from "react-router-dom";
+import styles from "./ProductPage.module.css";
+import { useState } from "react";
+import NumberInput from "../widgets/NumberInput";
 
 function ProductPage() {
     const { products, addToCart } = useOutletContext();
+    const [quantity, setQuantity] = useState(1);
     const params = useParams();
     const prodID = parseInt(params.prodID)
     const product = products.find(product => product.id === prodID);
 
     return (
-        <div>
-            <img src={product.image} alt="" />
-            <p>{product.title}</p>
-            <p>{product.price}</p>
-            <button onClick={() => addToCart(prodID)}>Add to Cart</button>
+        <div className={styles.productPage}>
+            <div className={styles.imageContainer}>
+                <img src={product.image} alt="" />
+            </div>
+            <div className={styles.descriptionContainer}>
+                <p className={styles.title}>{product.title}</p>
+                <p className={styles.price}>{product.price}</p>
+                <NumberInput number={quantity} setNumber={setQuantity} />
+                <button onClick={() => addToCart(prodID, quantity)}>Add to Cart</button>
+            </div>
         </div>
     )
 }
