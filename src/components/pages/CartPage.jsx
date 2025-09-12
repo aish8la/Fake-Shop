185ef4d4b254bcd2899e7de8947efe1a92c55e76
Aish Waheed <64914670+aish8la@users.noreply.github.com>
2025-09-12 22:14:10 +0500

    feat: add cart total feature and basic layout style to cart page

--- src/components/pages/CartPage.jsx
+++ src/components/pages/CartPage.jsx
@@ -4,13 +4,22 @@ import styles from "./CartPage.module.css";
 
 function CartPage() {
     const {cart} = useOutletContext();
+    const cartTotal = cart.reduce((total, current) => {
+        const itemPrice = parseInt(current.qty) * parseFloat(current.price);
+        return total + itemPrice;
+    }, 0)
 
     return (
         <div className={styles.cartPage}>
             <ul className={styles.cartItemContainer}>
                 {cart.map(item => <CartItem key={item.id} item={item}></CartItem>)}
             </ul>
-            
+            <div>
+                <dl>
+                    <dt>Total</dt>
+                    <dd>{cartTotal}</dd>
+                </dl>
+            </div>
         </div>
     )
 }
