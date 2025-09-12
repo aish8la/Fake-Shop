f388ad6531caf36f87a509c3e777a342d7589333
Aish Waheed <64914670+aish8la@users.noreply.github.com>
2025-09-12 23:08:20 +0500

    feat: change implementation of number input to accomodate for general usage

--- src/components/widgets/NumberInput.jsx
+++ src/components/widgets/NumberInput.jsx
@@ -2,22 +2,18 @@ import styles from "./NumberInput.module.css";
 
 function NumberInput({number, setNumber}) {
 
-    function changeNumber(modifier = 1, newNumber = null) {
-        setNumber(prev => {
-            if(newNumber) {
-                newNumber = parseInt(newNumber);
-            } else {
-                newNumber = parseInt(prev) + modifier;
-            }
-            return parseInt(newNumber > 99 || newNumber < 1 ? 1 : newNumber);
-        });
+    function changeNumber(newNumber) {
+        if(newNumber === "") return
+        newNumber = parseInt(newNumber)
+        if(newNumber > 99 || newNumber < 1) return
+        setNumber(newNumber);
     }
 
     return (
         <div className={styles.numberInput}>
-            <button onClick={() => changeNumber(-1)}>-</button>
-            <input type="number" value={number} onChange={(e) => changeNumber(0, e.target.value)} max="1" min="99"/>
-            <button onClick={() => changeNumber(1)}>+</button>
+            <button onClick={() => changeNumber(number - 1)}>-</button>
+            <input type="number" value={number} onChange={(e) => changeNumber(e.target.value)} max="1" min="99"/>
+            <button onClick={() => changeNumber(number + 1)}>+</button>
         </div>
     )
 }
