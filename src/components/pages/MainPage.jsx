568f5b7800cbb8d7236f41f486e604c28cd6f5b1
Aish Waheed <64914670+aish8la@users.noreply.github.com>
2025-09-13 06:58:05 +0500

    feat: add style to main page

--- src/components/pages/MainPage.jsx
+++ src/components/pages/MainPage.jsx
@@ -1,11 +1,15 @@
 import styles from "./MainPage.module.css";
 import hero from "../../assets/hero-img.jpg";
+import { Link } from "react-router-dom";
 
 function MainPage() {
 
     return <div>
-        <section>
+        <section className={styles.hero}>
             <img className={styles.heroImage} src={hero} alt="" />
+            <div className={styles.overlay}>
+                <Link to="shop">Go To Shop</Link>
+            </div>
         </section>
     </div>
 }
